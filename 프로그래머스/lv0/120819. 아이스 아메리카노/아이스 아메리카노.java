85298e2117268c6580149625599872bc12adbda2
lee na kyung <128464668+velyn666@users.noreply.github.com>
2023-09-11 20:17:15 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.03 ms, Memory: 81.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] solution(int money) {
+        int[] answer = new int[2];
+        for(;;){
+            if(money >= 5500) {
+                money = money - 5500;
+                answer[0] += 1;
+            } else {
+                answer[1] = money;
+                break;
+            }
+        }
+        return answer;
+    }
+}
