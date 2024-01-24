012e717eabfad589b4c4e08e5a594a727cfdaa8d
lee na kyung <128464668+O-Damnpr@users.noreply.github.com>
2024-01-24 11:13:27 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.02 ms, Memory: 84.5 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -1,21 +1,6 @@
 class Solution {
-    public int[] gcdsu(int a, int b) {
-        int[] answer = new int[2];
-
-          answer[0] = gcd(a,b);
-        answer[1] = (a*b)/answer[0];
-        return answer;
-    }
-    
-    public static int gcd(int p, int q)
-    {
-        if (q == 0) return p;
-        return gcd(q, p%q);
-    }
-    
     public int[] solution(int n, int m) {
         int[] answer = {};
-        answer = gcdsu(n,m);
         return answer;
     }
 }
